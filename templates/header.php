d17f748c1488f5eb5f7a7ebe8b2852fd3125fbad
Lucas de Oliveira <l.cassiano1612@gmail.com>
2017-11-18 19:07:32 -0200

    adicionado login no header e nova foto para banner

--- templates/header.php
+++ templates/header.php
@@ -8,7 +8,36 @@
             <li><a href="empresa.php">empresa</a></li>
             <li><a href="servicos.php">serviços</a></li>
             <li><a href="contato.php">contato</a></li>
-            <li><a class="area-cliente" href="#login">Área do cliente</a></li>
+            <li><a class="area-cliente" data-toggle="modal" data-target="#exampleModal" href="#login">Área do cliente</a></li>
         </ul>
     </nav>
 </header>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">E-mail</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Senha</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+            <button type="button" class="btn btn-primary">Login</button>
+            </div>
+        </div>
+    </div>
+</div>
