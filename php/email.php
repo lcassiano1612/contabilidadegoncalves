59b0eb58182d86ab89f56edb9ffaed3eb35b6594
Lucas de Oliveira <l.cassiano1612@gmail.com>
2017-11-20 09:06:08 -0200

    novo email

--- php/email.php
+++ php/email.php
@@ -4,45 +4,10 @@
     $assunto = "email de teste";
     $mensagem = "mensagem no email de teste";
 
+
     $email = "lucas.oliveira@ingressorapido.com.br";
     $headers = 'From: '.$email;
 
-    mail( $meu_email, $assunto, $mensagem, $headers );
+    mail( "atendimento@contabilidadegoncalves.com.br" , $assunto, $mensagem, $headers);
 
-    /*
-    $mail_to      = "l.cassiano1612@gmail.com";
-    $mail_subject = "Contato via formulario do site!";
-    $mail_message = "minha mensagem!";
-    $from_name = "Lucas de Oliveira";
-    $from_mail = "lucasoliveira@ingressorapido.com.br";
-
-    $encoding = "utf-8";
-
-    // Preferences for Subject field
-    $subject_preferences = array(
-        "input-charset" => $encoding,
-        "output-charset" => $encoding,
-        "line-length" => 76,
-        "line-break-chars" => "\r\n"
-    );
-
-    // Mail header
-    $header = "Content-type: text/html; charset=".$encoding." \r\n";
-    $header .= "From: ".$from_name." <".$from_mail."> \r\n";
-    $header .= "MIME-Version: 1.0 \r\n";
-    $header .= "Content-Transfer-Encoding: 8bit \r\n";
-    $header .= "Date: ".date("r (T)")." \r\n";
-    $header .= iconv_mime_encode("Subject", $mail_subject, $subject_preferences);
-
-    // Send mail
-    if(mail($mail_to, $mail_subject, $mail_message, $header) ){
-
-        echo "deu certo";
-
-    }else {
-
-        echo "nao deu certo";   
-
-    }
-    */
 ?>
