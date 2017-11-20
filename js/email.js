c58069ab9e74724c43dbc5c0542704b69a38b544
Lucas de Oliveira <l.cassiano1612@gmail.com>
2017-11-20 13:28:09 -0200

    add new ajax

--- js/email.js
+++ js/email.js
@@ -7,22 +7,10 @@ $( ".submit-form" ).click(function(e) {
     var email       = $("input[name=email]").val();
     var mensagem    = $("textarea[name=mensagem]").val();
 
-    $.ajax({
-        url: "../php/email.php", 
-        data: {
-            nome: nome,
-            telefone: telefone, 
-            email: email, 
-            mensagem: mensagem
-        },
-        success: function(result){
-            $("#div1").html(result);
-        }
+    $.post('../php/email.php',{ nome: nome, telefone: telefone, email: email, mensagem: mensagem },function(data){
+    
+        alert(data);
+    
     });
-
-    alert(nome);
-    alert(telefone);
-    alert(email);
-    alert(mensagem);
 
 });
