fac8d6c43597c0bda54c95acef3ac6adfb3258d4
Lucas de Oliveira <l.cassiano1612@gmail.com>
2017-11-20 13:55:43 -0200

    email

--- php/email.php
+++ php/email.php
@@ -1,16 +1,5 @@
 <?php
 
-    $assunto    = $nome." | ".$telefone;
-
-    $email      = "lucas.oliveira@ingressorapido.com.br";
-    $headers    = 'From: '.$email;
-
-    if(mail( "l.cassiano1612@gmail.com" , $assunto, $mensagem, $headers)){
-
-        echo "deu certo";
-
-    }
-
     $nome       = $_POST['nome'];
     $telefone   = $_POST['telefone'];
     $email      = $_POST['email'];
