fb939de0fe930164a1f2354d9139eaedc0388a47
Lucas de Oliveira <l.cassiano1612@gmail.com>
2017-11-20 13:51:17 -0200

    novo envio email

--- php/email.php
+++ php/email.php
@@ -1,10 +1,4 @@
 <?php
-    
-    $nome       = $_POST['nome'];
-    $telefone   = $_POST['telefone'];
-    $email      = $_POST['email'];
-    $mensagem   = $_POST['mensagem'];
-
 
     $assunto    = $nome." | ".$telefone;
 
@@ -15,6 +9,41 @@
 
         echo "deu certo";
 
+    }
+
+    $nome       = $_POST['nome'];
+    $telefone   = $_POST['telefone'];
+    $email      = $_POST['email'];
+    $mensagem   = $_POST['mensagem'];
+    
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    
+    /*abaixo contém os dados que serão enviados para o email
+    cadastrado para receber o formulário*/
+    
+    $corpo = "Formulário enviado\n";
+    $corpo .= "Nome: " . $nome . "\n";
+    $corpo .= "Telefone: " . $telefone . "\n";
+    $corpo .= "Email: " . $email . "\n";
+    $corpo .= "Comentários: " . $mensagem . "\n";
+    
+    $email_to = 'atendimento@contabilidadegoncalves.com.br';
+    //não esqueça de substituir este email pelo seu.
+    
+    $subject = "Contato via site";
+
+    $status = mail($email_to, $subject, $corpo, $headers);
+    //enviando o email.
+    
+    if ($status) {
+        echo "<script> alert('Formulário enviado com sucesso!'); </script>";
+    //mensagem de form enviado com sucesso.
+    
+    } else {
+        echo "<script> alert('Falha ao enviar o Formulário.'); </script>";
+    //mensagem de erro no envio. 
+    
     }
 
 ?>
